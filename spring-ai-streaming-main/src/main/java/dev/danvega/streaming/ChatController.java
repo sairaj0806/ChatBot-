6971ecabc055e9b4fa9e5b99393137d219c300e1
sairaj0806 <166358164+sairaj0806@users.noreply.github.com>
2025-03-28 05:56:16 +0530

    Add files via upload

---
+++ spring-ai-streaming-main/src/main/java/dev/danvega/streaming/ChatController.java
@@ -0,0 +1,33 @@
+package dev.danvega.streaming;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+
+@RestController
+@CrossOrigin
+public class ChatController {
+
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+    @PostMapping("/chat")
+    public String chat(@RequestParam String message) {
+        return chatClient.prompt()
+                .user(message)
+                .call()
+                .content();
+    }
+
+    @GetMapping("/stream")
+    public Flux<String> chatWithStream(@RequestParam String message) {
+        return chatClient.prompt()
+                .user(message)
+                .stream()
+                .content();
+    }
+
+}
